764d35d8e52ce983916cd04d78aef9323fd918cf
Julien Henchoz <jhenchoz@cobweb.ch>
2018-10-04 13:49:00 +0200

    Improved networking of services

--- pusher/server.js
+++ pusher/server.js
@@ -2,24 +2,25 @@ var amqp = require('amqplib/callback_api');
 console.info("Starting annoying pusher!");
 
 var connect = () => {
-amqp.connect('amqp://127.0.0.1', function(err, conn) {
-  console.log("Trying to connect to RabbitMQ...");
-  if (err) {
-    setTimeout(connect, 1000);
-    return;
-  }
-  conn.createChannel(function(err, ch) {
-    setInterval(() => {
-    var q = 'hello';
+    amqp.connect('amqp://rabbitmq', function (err, conn) {
+        console.log("Trying to connect to RabbitMQ...");
+        if (err) {
+            console.error(err);
+            setTimeout(connect, 1000);
+            return;
+        }
+        conn.createChannel(function (err, ch) {
+            setInterval(() => {
+                var q = 'hello';
 
-    ch.assertQueue(q, {durable: false});
-    // Note: on Node 6 Buffer.from(msg) should be used
-    ch.sendToQueue(q, new Buffer('Hello World!'));
-    console.log(" [x] Sent 'Hello World!'");
-    }, 2000);
+                ch.assertQueue(q, {durable: false});
+                // Note: on Node 6 Buffer.from(msg) should be used
+                ch.sendToQueue(q, new Buffer('Hello World!'));
+                console.log(" [x] Sent 'Hello World!'");
+            }, 2000);
 
-  });
-});
+        });
+    });
 };
 
 connect();
