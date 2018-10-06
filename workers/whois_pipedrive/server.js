071d44a429db189e7f1ed3fa38d323a228e5c772
Julien Henchoz <jhenchoz@cobweb.ch>
2018-10-06 19:30:19 +0200

    Added todo

--- workers/whois_pipedrive/server.js
+++ workers/whois_pipedrive/server.js
@@ -3,6 +3,7 @@ const pipedrive = require('./helpers/pipedrive');
 const format = require('./helpers/format')
 console.info("Starting " + config.workerName + " service!");
 
+// TODO : add listening of queue "whois" and push the result to queue "slack_outbound"
 var init = async () => {
     var apiResults = await pipedrive.findPersons('Sudan');
     var suggestions = [];
